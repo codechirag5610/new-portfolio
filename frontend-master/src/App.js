47c2342a0d6650ea09d8a1338b9da4f9d45b292d
codechirag5610 <sharmachirag347@gmail.com>
2025-10-18 01:49:17 +0530

    feat: updating all the handles and adding footer

--- frontend-master/src/App.js
+++ frontend-master/src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/index';
+import { Navbar, Footer } from './components/index';
 import { ThemeProvider } from './context/ThemeContext';
 import HomePage from './pages/HomePage/HomePage';
 import AboutPage from './pages/AboutPage/AboutPage';
@@ -14,14 +14,17 @@ function App() {
       <Router>
         <div className="App">
           <Navbar />
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/case-studies" element={<CaseStudiesPage />} />
-              <Route path="/case-studies/:slug" element={<CaseStudyDetail />} />
-            </Routes>
-          </AnimatePresence>
+          <main className="app__main-content">
+            <AnimatePresence mode="wait">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/case-studies" element={<CaseStudiesPage />} />
+                <Route path="/case-studies/:slug" element={<CaseStudyDetail />} />
+              </Routes>
+            </AnimatePresence>
+          </main>
+          <Footer />
         </div>
       </Router>
     </ThemeProvider>
